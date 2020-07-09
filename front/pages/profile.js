45f8798ffa12516f04862e226ec20bfc2863356b
jungsikjeong <wndtlr1024@gmail.com>
2020-07-10 03:25:16 +0900

    2-1 _app.js로 레이아웃 분리시키기

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,20 +1,9 @@
 import React from "react";
-import AppLayout from "../components/AppLayout";
-import Head from "next/head";
 
 const profile = () => {
   return (
     <>
-      <Head>
-        <title>NodeBird</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.23.1/antd.css"
-        />
-      </Head>
-      <AppLayout>
-        <div>프로필</div>
-      </AppLayout>
+      <div>프로필</div>
     </>
   );
 };
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,6 +1,4 @@
 import React, { useState, useCallback } from "react";
-import AppLayout from "../components/AppLayout";
-import Head from "next/head";
 import { Form, Input, Checkbox, Button } from "antd";
 
 const signup = () => {
@@ -18,8 +16,8 @@ const signup = () => {
   };
 
   const [id, onChangeId] = useInput("");
-  const [nick, setNick] = useState("");
-  const [password, setPassword] = useState("");
+  const [nick, onChangeNick] = useInput("");
+  const [password, onChangePassword] = useInput("");
 
   const onSubmit = useCallback(
     (e) => {
@@ -33,14 +31,6 @@ const signup = () => {
     },
     [password, passwordCheck, term]
   );
-
-  const onChangeNick = (e) => {
-    setNick(e.target.value);
-  };
-
-  const onChangePassword = (e) => {
-    setPassword(e.target.value);
-  };
 
   // 이 함수는 passwordCheck를 칠때마다 발생하는데, password랑 같은지 체크해줌
   const onChangePasswordCheck = useCallback(
@@ -58,71 +48,62 @@ const signup = () => {
 
   return (
     <>
-      <Head>
-        <title>NodeBird</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.23.1/antd.css"
-        />
-      </Head>
-      <AppLayout>
-        <Form onSubmit={onSubmit} style={{ padding: 10 }}>
-          <div>
-            <label htmlFor="user-id">아이디</label>
-            <br />
-            <Input name="user-id" value={id} required onChange={onChangeId} />
-          </div>
-          <div>
-            <label htmlFor="user-nick">닉네임</label>
-            <br />
-            <Input
-              name="user-nick"
-              value={nick}
-              required
-              onChange={onChangeNick}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-password">비밀번호</label>
-            <br />
-            <Input
-              name="user-password"
-              type="password"
-              value={password}
-              required
-              onChange={onChangePassword}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-pass">비밀번호체크</label>
-            <br />
-            <Input
-              name="user-pass-check"
-              type="password"
-              value={passwordCheck}
-              required
-              onChange={onChangePasswordCheck}
-            />
-            {passwordError && (
-              <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
-            )}
-          </div>
-          <div>
-            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
-              제로초 말을 잘 들을 것을 동의합니다.
-            </Checkbox>
-            {termError && (
-              <div style={{ color: "red" }}>약관에 동의하셔야 합니다.</div>
-            )}
-          </div>
-          <div style={{ marginTop: 10 }}>
-            {/* antd에선type="submit"하려면 htmlType을 써줘야함 */}
-            <Button type="primary" htmlType="submit">
-              가입하기
-            </Button>
-          </div>
-        </Form>
-      </AppLayout>
+      <Form onSubmit={onSubmit} style={{ padding: 10 }}>
+        <div>
+          <label htmlFor="user-id">아이디</label>
+          <br />
+          <Input name="user-id" value={id} required onChange={onChangeId} />
+        </div>
+        <div>
+          <label htmlFor="user-nick">닉네임</label>
+          <br />
+          <Input
+            name="user-nick"
+            value={nick}
+            required
+            onChange={onChangeNick}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password">비밀번호</label>
+          <br />
+          <Input
+            name="user-password"
+            type="password"
+            value={password}
+            required
+            onChange={onChangePassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-pass">비밀번호체크</label>
+          <br />
+          <Input
+            name="user-pass-check"
+            type="password"
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordCheck}
+          />
+          {passwordError && (
+            <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
+          )}
+        </div>
+        <div>
+          <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+            제로초 말을 잘 들을 것을 동의합니다.
+          </Checkbox>
+          {termError && (
+            <div style={{ color: "red" }}>약관에 동의하셔야 합니다.</div>
+          )}
+        </div>
+        <div style={{ marginTop: 10 }}>
+          {/* antd에선type="submit"하려면 htmlType을 써줘야함 */}
+          <Button type="primary" htmlType="submit">
+            가입하기
+          </Button>
+        </div>
+      </Form>
     </>
   );
 };
